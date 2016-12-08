3a5528811fa715f23ac86e183332c30a8ca2dd8a
Blackcop\PRINCE <prince.pandey7@gmail.com>
2016-12-08 21:32:59 +0530

    test

--- js/1.5.8/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	 $('.packageCheck').click(function() {
-        if (!$(this).is(':checked')) {
-            return confirm("Are you sure?");
-        }
-    });
-	
-})
