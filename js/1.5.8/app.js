58f7d9f8104de87b7f0107b566945aa840ea1a6c
Blackcop\PRINCE <prince.pandey7@gmail.com>
2016-12-12 22:59:46 +0530

    work in progress.

--- js/1.5.8/app.js
+++ js/1.5.8/app.js
@@ -99,6 +99,113 @@ app.config(['$routeProvider',function($routeProvider) {
   });
 }]);
 
+
+
+
+
+app.controller('appController', ['$scope', function ($scope) {
+    $scope.starRating1 = 4;
+    $scope.starRating2 = 5;
+    $scope.starRating3 = 2;
+    $scope.hoverRating1 = $scope.hoverRating2 = $scope.hoverRating3 = 0;
+
+    $scope.click1 = function (param) {
+        console.log('Click(' + param + ')');
+    };
+
+    $scope.mouseHover1 = function (param) {
+        console.log('mouseHover(' + param + ')');
+        $scope.hoverRating1 = param;
+    };
+
+    $scope.mouseLeave1 = function (param) {
+        console.log('mouseLeave(' + param + ')');
+        $scope.hoverRating1 = param + '*';
+    };
+
+    $scope.click3 = function (param) {
+        console.log('Click');
+    };
+
+    $scope.mouseHover3 = function (param) {
+        console.log('mouseHover(' + param + ')');
+        $scope.hoverRating3 = param;
+    };
+
+    $scope.mouseLeave3 = function (param) {
+        console.log('mouseLeave(' + param + ')');
+        $scope.hoverRating3 = param + '*';
+    };
+}]);
+
+app.directive('starRating', function () {
+    return {
+        scope: {
+            rating: '=',
+            maxRating: '@',
+            readOnly: '@',
+            click: "&",
+            mouseHover: "&",
+            mouseLeave: "&"
+        },
+        restrict: 'EA',
+        template:
+            "<div style='display: inline-block; margin: 0px; padding: 0px; cursor:pointer;' ng-repeat='idx in maxRatings track by $index'> \
+                    <img ng-src='{{((hoverValue + _rating) <= $index) && \"http://www.codeproject.com/script/ratings/images/star-empty-lg.png\" || \"http://www.codeproject.com/script/ratings/images/star-fill-lg.png\"}}' \
+                    ng-Click='isolatedClick($index + 1)' \
+                    ng-mouseenter='isolatedMouseHover($index + 1)' \
+                    ng-mouseleave='isolatedMouseLeave($index + 1)'></img> \
+            </div>",
+        compile: function (element, attrs) {
+            if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
+                attrs.maxRating = '5';
+            };
+        },
+        controller: function ($scope, $element, $attrs) {
+            $scope.maxRatings = [];
+
+            for (var i = 1; i <= $scope.maxRating; i++) {
+                $scope.maxRatings.push({});
+            };
+
+            $scope._rating = $scope.rating;
+            
+            $scope.isolatedClick = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope.rating = $scope._rating = param;
+                $scope.hoverValue = 0;
+                $scope.click({
+                    param: param
+                });
+            };
+
+            $scope.isolatedMouseHover = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = 0;
+                $scope.hoverValue = param;
+                $scope.mouseHover({
+                    param: param
+                });
+            };
+
+            $scope.isolatedMouseLeave = function (param) {
+                if ($scope.readOnly == 'true') return;
+
+                $scope._rating = $scope.rating;
+                $scope.hoverValue = 0;
+                $scope.mouseLeave({
+                    param: param
+                });
+            };
+        }
+    };
+});
+
+
+
+
 app.run(['$rootScope','$location', '$routeParams', function($rootScope, $location, $routeParams) {
     $rootScope.$on('$routeChangeSuccess', function(e, current, pre) {
       console.log('Current route name: ' + $location.path());
@@ -115,6 +222,17 @@ app.controller('TabController', ['$scope', function($scope) {
       return $scope.tab === tabNum;
     };
 }]);
+
+app.controller('Tab1Controller', ['$scope', function($scope) {
+    $scope.tab = 1;
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+}]);
+
 
 
 app.controller('topKeywordsController', function($scope){
@@ -200,7 +318,7 @@ app.controller('dropdownCtrl', ['$scope','CustomerService', function($scope, Cus
   
 
 
-  $scope.submitBuyerData = function (person, resultVarName)
+  $scope.submitBuyerData = function (person, resultVarName, $http)
   {
     var config = {
       params: {
